347b1852d8ee07f673d7357c5ffb6cbad5ca0aef
Kevin Reynen <kreynen@gmail.com>
2017-05-18 12:54:51 -0600

    updated embed_templates to beta2

--- embed_templates/embed_templates.api.php
+++ embed_templates/embed_templates.api.php
@@ -16,8 +16,11 @@
  *   - submission_callback: This callback fires when the embed form is saved.
  *       Useful for doing things before an entity is created/deleted.
  *   - renderer: Machine name of the renderer to use when displaying template.
+ *   - default_status: The status your embed type that is set as default on the
+ *       embed add form.
  *   - path: The path of the module where the template file is located.
- *   - custom_template: You need to enter the relative path to your template file from the module root.
+ *   - custom_template: You need to enter the relative path to your template file
+ *       from the module root.
  *
  * @return array
  */
@@ -29,9 +32,39 @@ function hook_embed_templates_types() {
     'form_callback' => 'my_pixel_form',
     'submission_callback' => 'my_pixel_form_submit',
     'renderer' => 'block',
+    'default_status' => 'unpublished',
     'path' => drupal_get_path('module', 'my_module'),
     'template' => 'templates/tracking-pixel-custom-service',
   );
 
   return $custom_embeds;
+}
+
+/**
+ * Add custom statuses for embeds.
+ *
+ * array $custom_status
+ *   - label: The human-readable name of your status.
+ *   - operation_callback: Callback for if someone selects a change to this
+ *       status. NULL if no operation.
+ *   - bulk_operation_label: Label for Embed Overview bulk operation select list.
+ *       NULL if no operation.
+ *   - confirmation_label: Label for confirmation screen.
+ *   - add_permission: String of the permission to check for when changing statuses
+ *       This should map to your hook_permission(). NULL if no permission needed.
+ *
+ * @return array
+ */
+function hook_embed_templates_status() {
+  $custom_status = array();
+
+  $custom_status['in_review'] = array(
+    'label' => 'In Review',
+    'operation_callback' => 'in_review_operation_callback',
+    'bulk_operation_label' => 'Mark Embeds In Needs Review State',
+    'confirmation label' => 'mark as "Needs Review"',
+    'add_permission' => 'mark embeds in review',
+  );
+
+  return $custom_status;
 }
--- embed_templates/modules/et_beans/templates/embed-templates-slate-bean.tpl.php
+++
@@ -1,2 +0,0 @@
-<?php print 'Slate Form ID: ' . $variables['slate_id']; ?>
-<h3>Placeholder for Slate Form</h3>
