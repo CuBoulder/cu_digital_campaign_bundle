b9197c8fa86e464c60e076d3ab7511ba5760ed14
Kevin Reynen <kreynen@gmail.com>
2017-05-23 06:05:32 -0600

    updated et_forms to latest dev snapshot

--- et_slate_form/templates/slate-form.tpl.php
+++ et_slate_form/templates/slate-form.tpl.php
@@ -1,4 +1,4 @@
-<div id="form_<?php print check_plain($variables['account_id']); ?>">Loading...</div>
+<div id="form_<?php print check_plain($variables['form_id']); ?>">Loading...</div>
 <script>/*<![CDATA[*/
   var script = document.createElement('script');
   var div = jQuery('.field-item'),
@@ -7,9 +7,10 @@
     }).get(0);
 
   if (typeof comment !== "undefined") {
-    var program = comment.nodeValue.split(":");
+    var options = comment.nodeValue.split(":");
+    var optionsquery = '&sys:field:' + options[0] + '=' + options[1] + ';
   } else {
-    var program = "";
+    var options = "";
   }
-  script.async = 1; script.src = 'https://grad.apply.colorado.edu/register/?id=<?php print check_plain($variables['account_id']); ?>&output=embed&sys:field:department=' + program[1] + '&div=form_<?php print check_plain($variables['account_id']); ?>' + ((location.search.length > 1) ? '&' + location.search.substring(1) : ''); var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);
+  script.async = 1; script.src = 'https://<?php print check_plain($variables['slate_domain']); ?>/register/?id=<?php print check_plain($variables['form_id']); ?>&output=embed' + programquery + '&div=form_<?php print check_plain($variables['form_id']); ?>' + ((location.search.length > 1) ? '&' + location.search.substring(1) : ''); var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);
   /*]]>*/</script>
