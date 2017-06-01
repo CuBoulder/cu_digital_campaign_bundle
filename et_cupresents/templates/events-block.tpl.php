631ae8ea2652de4bbaa607232ba954115d557dce
Alex Finnarn <alexfinnarn@gmail.com>
2017-06-01 14:52:25 -0600

    adding CU Presents embed template

---
+++ et_cupresents/templates/events-block.tpl.php
@@ -0,0 +1,3 @@
+<div class="cupresents-event-block-wrapper">
+  <script src="//bridge.cupresents.org/rss/production?format=js&filters%5bseries%5d%5b%5d=<?php print check_plain($variables['series']); ?>&include_custom=<?php print check_plain($variables['include_custom']); ?>"></script>
+</div>
