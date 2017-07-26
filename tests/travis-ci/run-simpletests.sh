a756d741a1e3957ded920c49530fb5d6ed71bf84
Alex Finnarn <alexfinnarn@gmail.com>
2017-07-26 15:33:19 -0600

    run simpletests too

--- tests/travis-ci/run-simpletests.sh
+++ tests/travis-ci/run-simpletests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-OUTPUT="$( php ./scripts/run-tests.sh --url http://127.0.0.1:8080 'Embed Templates')"
+OUTPUT=`php $HOME/drupal/scripts/run-tests.sh --url http://127.0.0.1:8080 "Embed Templates"`
 echo "${OUTPUT}"
 
 if [[ $OUTPUT == *"fail,"* ]]; then
