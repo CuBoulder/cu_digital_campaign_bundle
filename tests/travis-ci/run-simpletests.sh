f6679fc271b4c8b3ffb1b78d9064561f75a0260c
Alex Finnarn <alexfinnarn@gmail.com>
2017-07-26 17:25:33 -0600

    only remove PHP module

--- tests/travis-ci/run-simpletests.sh
+++ tests/travis-ci/run-simpletests.sh
@@ -8,7 +8,7 @@ echo "${OUTPUT}"
 OUTPUT=`php ./scripts/run-tests.sh --url http://127.0.0.1:8080 --php ~/.phpenv/versions/$(phpenv version-name)/bin/php "Embed Templates"`
 echo "${OUTPUT}"
 
-if [[ $OUTPUT == *"fail,"* ]]; then
+if [[ $OUTPUT == *fail,* ]]; then
   echo 'failed'
   exit 1
 else
