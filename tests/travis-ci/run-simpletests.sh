8fc5afc62ea4b4ff5c8c3a057cc012bb42c1120f
Alex Finnarn <alexfinnarn@gmail.com>
2017-07-26 16:02:36 -0600

    run simpletests too

--- tests/travis-ci/run-simpletests.sh
+++ tests/travis-ci/run-simpletests.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-chmod +x $HOME/drupal/scripts/run-tests.sh
-OUTPUT=`php $HOME/drupal/scripts/run-tests.sh --url http://127.0.0.1:8080 "Embed Templates"`
+#chmod +x $HOME/drupal/scripts/run-tests.sh
+cd ../../../../../../
+FOO=`pwd`
+echo "${OUTPUT}"
+
+OUTPUT=`php ./scripts/run-tests.sh --url http://127.0.0.1:8080 "Embed Templates"`
 echo "${OUTPUT}"
 
 if [[ $OUTPUT == *"fail,"* ]]; then
