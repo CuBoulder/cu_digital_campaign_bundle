fa0adaf8d87284f975020c63e6cb1e35709c1ad5
Alex Finnarn <alexfinnarn@gmail.com>
2017-08-08 16:14:58 -0600

    printing out variable to debug

--- tests/travis-ci/express-tests.sh
+++ tests/travis-ci/express-tests.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 # Check commit message for !==express to skip test run.
+# Need to go two commit messages back because Travis merges PR branch into default branch.
+OUTPUT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep '!==express' )"
+echo "${OUTPUT}"
