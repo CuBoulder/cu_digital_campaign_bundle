db28e2772827963dce7e15fa35cdf29073b10765
Alex Finnarn <alexfinnarn@gmail.com>
2017-08-09 10:32:20 -0600

    tag with @testing not the end

--- tests/travis-ci/bundle-tests.sh
+++ tests/travis-ci/bundle-tests.sh
@@ -2,15 +2,15 @@
 
 # Check commit message for @ to only run tag for bundle.
 # Need to go two commit messages back because Travis merges PR branch into default branch.
-BUNDLE_COMMIT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep @)"
+BUNDLE_COMMIT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep '^\@')"
 echo "${BUNDLE_COMMIT}"
-echo "$(git log -2 --pretty=%B)"
 
 # If @ is in the commit message, then only run tests with that tag.
 if [ "${BUNDLE_COMMIT}" ]; then
   echo "Found @ keyword."
 
-  # Get tag from commit message. Oh yeah!
+  # Get tag from commit message. Oh yeah! More nonsense!!!
+  echo ${BUNDLE_COMMIT} | grep '@^\S'
 
   echo "Not running Express tests."
 else
