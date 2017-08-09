0132d402b85a081c308ff145710970e08ab08cf6
Alex Finnarn <alexfinnarn@gmail.com>
2017-08-09 10:16:34 -0600

    testing with tag @testing

--- tests/travis-ci/express-tests.sh
+++ tests/travis-ci/express-tests.sh
@@ -2,12 +2,15 @@
 
 # Check commit message for !==express to skip test run.
 # Need to go two commit messages back because Travis merges PR branch into default branch.
-OUTPUTTT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep !==express)"
-echo "${OUTPUTTT}"
+EXPRESS_COMMIT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep !==express)"
+echo "${EXPRESS_COMMIT}"
 
 # If !==express is in the commit message, then don't run Express tests.
-if [ "${OUTPUTTT}" ]; then
-  echo "found !==express"
+if [ "${EXPRESS_COMMIT}" ]; then
+  echo "Found !==express keyword."
+  echo "Not running Express tests."
 else
-  echo "did not find the keywords"
+  echo "No keyword found."
+  echo "Running Express tests."
+  ./bin/behat --config behat.yml --verbose
 fi
