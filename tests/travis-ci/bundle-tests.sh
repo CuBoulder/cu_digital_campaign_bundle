b21850102e8f6d1672a0d857146d4243acf4902c
Alex Finnarn <alexfinnarn@gmail.com>
2017-08-09 11:20:05 -0600

    testing with @test tag and !==express

--- tests/travis-ci/bundle-tests.sh
+++ tests/travis-ci/bundle-tests.sh
@@ -2,17 +2,14 @@
 
 # Check commit message for @ to only run tag for bundle.
 # Need to go two commit messages back because Travis merges PR branch into default branch.
-BUNDLE_COMMIT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep '^\@')"
+BUNDLE_COMMIT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | cut -f2 -d'@' | cut -f1 -d' ')"
 echo "${BUNDLE_COMMIT}"
 
 # If @ is in the commit message, then only run tests with that tag.
 if [ "${BUNDLE_COMMIT}" ]; then
   echo "Found @ keyword."
-
-  # Get tag from commit message. Oh yeah! More nonsense!!!
-  echo ${BUNDLE_COMMIT} | grep '@^\S'
-
-  echo "Not running Express tests."
+  echo "Only running tagged tests."
+  ./bin/behat --config behat.bundle.yml --verbose --tags "@${BUNDLE_COMMIT}"
 else
   echo "No keyword found."
   echo "Running all bundle tests."
