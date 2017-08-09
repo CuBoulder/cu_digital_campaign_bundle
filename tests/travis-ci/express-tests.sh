f263864da747a7a0b23861a6aad6570717bbb04e
Alex Finnarn <alexfinnarn@gmail.com>
2017-08-09 09:19:00 -0600

    testing script to check for !==express

--- tests/travis-ci/express-tests.sh
+++ tests/travis-ci/express-tests.sh
@@ -2,5 +2,12 @@
 
 # Check commit message for !==express to skip test run.
 # Need to go two commit messages back because Travis merges PR branch into default branch.
-OUTPUT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep '!==express' )"
-echo "${OUTPUT}"
+OUTPUTTT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep !==express)"
+echo "${OUTPUTTT}"
+
+# If !==express is in the commit message, then don't run Express tests.
+if [ $(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep !==express) ]; then
+  echo "found !==express"
+else
+  echo "did not find it"
+fi
