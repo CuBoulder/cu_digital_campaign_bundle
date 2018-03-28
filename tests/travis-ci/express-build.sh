36ad802ab70d419fa785c43a04bc620c9edc5d6c
Alex Finnarn <alexfinnarn@gmail.com>
2018-03-28 15:28:33 -0600

    try caching site build !==express

--- tests/travis-ci/express-build.sh
+++ tests/travis-ci/express-build.sh
@@ -1,46 +1,22 @@
 #!/usr/bin/env bash
 
-# Get latest tag for Express repo
-git fetch --tags
-OUTPUT="$(git describe --tags `git rev-list --tags --max-count=1`)"
-echo "${OUTPUT}"
+EXPRESS_COMMIT="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep '===build')"
+echo "${EXPRESS_COMMIT}"
 
-# Checkout that release. The bundle will be deployed to prod with the latest Express release.
-git checkout $OUTPUT
+# Build Express if no db export or commit is "merged into dev".
+if [ ! -f $HOME/cache/express.sql ] || [  "${EXPRESS_COMMIT}" ]; then
 
-# If latest tag file does not exist, then create one and install site.
-if [ ! -f $HOME/cache/latest-tag.txt ]; then
-    echo "Latest tag not found!"
+  # Install site like normal.
+  echo Installing Express...
+  drush si express --db-url=mysql://root:@127.0.0.1/drupal --account-name=admin --account-pass=admin --site-mail=admin@example.com --site-name="Express" --yes
 
-    touch $HOME/cache/latest-tag.txt
-    echo "${OUTPUT}" >> $HOME/cache/latest-tag.txt
-    echo "Latest tag saved."
-
-    drush si express --db-url=mysql://root:@127.0.0.1/drupal --account-name=admin --account-pass=admin --site-mail=admin@example.com --site-name="Express" --yes
-    drush sql-dump --result-file=$HOME/cache/express.sql
-    echo "Backup exported."
-
-# If the tag does exist, check to see if the latest tag is the same.
+  # Export db so it can be imported after every suite run.
+  # Test runs that fill up the db with nodes can impact other tests.
+  echo Exporting database...
+  drush sql-dump --result-file=$HOME/cache/express.sql
 else
-    echo "Latest tag file found!"
-    latestTag=`cat $HOME/cache/latest-tag.txt`
 
-    # Check if tags match
-    if [ "$latestTag" =  "$OUTPUT" ]; then
-        echo "Tags match."
-
-        drush sql-cli < $HOME/cache/express.sql
-        echo "Backup imported."
-    else
-        echo "Tags do not match."
-
-        drush si express --db-url=mysql://root:@127.0.0.1/drupal --account-name=admin --account-pass=admin --site-mail=admin@example.com --site-name="Express" --yes
-        drush sql-dump --result-file=$HOME/cache/express.sql
-        echo "Backup exported."
-
-        rm $HOME/cache/latest-tag.txt
-        touch $HOME/cache/latest-tag.txt
-        echo "${OUTPUT}" >> $HOME/cache/latest-tag.txt
-        echo "Latest tag saved."
-    fi
+  # Import db if it is already built.
+  echo Importing Express database...
+  drush sql-cli < $HOME/cache/express.sql
 fi
