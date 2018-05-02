249d92f400d76a526aca46a4e72f38af21af61d7
Alex Finnarn <alexfinnarn@gmail.com>
2018-05-02 13:51:11 -0600

    update bunch of stuff

--- cu_doubleclick/templates/cu-doubleclick.tpl.php
+++ cu_doubleclick/templates/cu-doubleclick.tpl.php
@@ -8,9 +8,7 @@ Creation Date: <?php print check_plain($variables['creation']); ?>.
 <script type="text/javascript">
   var axel = Math.random() + "";
   var a = axel * 10000000000000;
-  document.write('<iframe src="https://<?php print check_plain($variables['account_id']); ?>.fls.doubleclick.net/activityi;src=<?php print check_plain($variables['account_id']); ?>;type=<?php print check_plain($variables['type']); ?>;cat=<?php print check_plain($variables['category']); ?>;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
-</script>
-<noscript>
-  <iframe src="https://<?php print check_plain($variables['account_id']); ?>.fls.doubleclick.net/activityi;src=<?php print check_plain($variables['account_id']); ?>;type=<?php print check_plain($variables['type']); ?>;cat=<?php print check_plain($variables['category']); ?>;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
-</noscript>
+  document.write('<iframe src="https://<?php print check_plain($variables['account_id']); ?>.fls.doubleclick.net/activityi;src=<?php print check_plain($variables['account_id']); ?>;type=<?php print check_plain($variables['type']); ?>;cat=<?php print check_plain($variables['category']); ?>;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>'); </script>
+<iframe src="https://<?php print check_plain($variables['account_id']); ?>.fls.doubleclick.net/activityi;src=<?php print check_plain($variables['account_id']); ?>;type=<?php print check_plain($variables['type']); ?>;cat=<?php print check_plain($variables['category']); ?>;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+
 <!-- End of DoubleClick Floodlight Tag: Please do not remove -->
